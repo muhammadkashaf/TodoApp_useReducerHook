80e462df7faaf1cca1c30b526783f5779018ac2d
muhammadkashaf <kashafahmed72@gmail.com>
2020-07-29 23:09:52 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,60 +1,13 @@
-import React, { useReducer, useState } from "react";
-import Todo from "./Todo";
+import React from "react";
+import "./App.css";
 
-export const ACTIONS = {
-  ADD_TODO: "add-todo",
-  TOGGLE_TODO: "toggle-todo",
-  DELETE_TODO: "delete-todo",
-};
-
-function reducer(todos, action) {
-  switch (action.type) {
-    case ACTIONS.ADD_TODO:
-      return [...todos, newTodo(action.payload.name)];
-    case ACTIONS.TOGGLE_TODO:
-      return todos.map((todo) => {
-        if (todo.id === action.payload.id) {
-          return { ...todo, complete: !todo.complete };
-        }
-        return todo
-      });
-    case ACTIONS.DELETE_TODO:
-      return todos.filter(todo => todo.id !== action.payload.id);
-
-    default:
-      return todos
-  }
-}
-
-function newTodo(name) {
-  return { id: Date.now(), name: name, complete: false };
-}
+import { initNotification } from "./services/firebaseService";
 
 export default function App() {
-  const [todos, dispatch] = useReducer(reducer, []);
-  const [name, setName] = useState("");
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } });
-    setName("");
-  }
-
-  console.log(todos);
-
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </form>
-
-      {todos.map((todo) => {
-        return <Todo key={todo.id} todo={todo} dispatch={dispatch} />;
-      })}
-    </>
+    <div className="App">
+      <h1>App</h1>
+      <button onClick={initNotification}>Configure Notifications</button>
+    </div>
   );
 }
